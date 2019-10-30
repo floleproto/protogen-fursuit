20860a494cb215035694c565223f9850bd97105c
Beep Boop de compagnie <beepboop@proto.yif>
2019-10-30 15:39:12 +0100

    Initial commit

---
+++ web/beepboop/index.py
@@ -0,0 +1,11 @@
+from flask import Flask, escape, request, render_template
+
+app = Flask(__name__)
+
+#if __name__ == "__main__":
+#  app.run(host='0.0.0.0')
+
+@app.route('/')
+def main():
+   #return 'Hello'
+   return render_template("main.html")
